a234c397d16a62f4cccb350d39d3cddb58c078c6
Chris Baiamonte <baiamonte.chris@gmail.com>
2021-11-15 15:29:14 -0500

    Remove early test code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import fetch from "node-fetch";
 import Config from "./config.js";
 import GithubRepository from "./github/githubRepository.js";
 import Secrets from "./secrets.js";
-
-const body = { a: 1 };
-const response = await fetch("https://httpbin.org/post", {
-  method: "post",
-  body: JSON.stringify(body),
-  headers: { "Content-Type": "application/json" },
-});
 
 const secrets = await new Secrets().initialize();
 const config = await new Config().initialize();
